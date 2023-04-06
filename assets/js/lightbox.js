7182cc3b2738253e8260f5eff38416b441f7b5a2
“r-ferriergit config --global user.email “rosemary.ferrier@gmail.com <rosemary.ferrier@gmail.com>
2023-04-06 16:29:09 +0100

    fix js broken by merge

--- assets/js/lightbox.js
+++ assets/js/lightbox.js
@@ -9,16 +9,15 @@ document.addEventListener("DOMContentLoaded", () => {
   const elements = document.querySelectorAll("img.timeline-image__image");
   const lightbox = document.querySelectorAll("div.lightbox")[0];
   elements.forEach((element) => {
-    element.addEventListener("click", function (event) {
+    element.parentElement.addEventListener("click", (event) => {
       event.preventDefault();
-      const title = this.getAttribute("title");
+      const title = element.getAttribute("title");
       const src =
-        this.getAttribute("src").substr(
-          0,
-          this.getAttribute("src").lastIndexOf("/")
-        ) +
+        element
+          .getAttribute("src")
+          .substring(0, element.getAttribute("src").lastIndexOf("/")) +
         "/" +
-        this.getAttribute("title");
+        element.getAttribute("title");
       lightbox.innerHTML =
         '<a class="lightbox__close"></a><div class="lightbox__image" style="background: url(\'' +
         src +
@@ -27,9 +26,9 @@ document.addEventListener("DOMContentLoaded", () => {
         '" ><img class="lightbox__hidden-image" src="' +
         src +
         '" alt="' +
-        this.getAttribute("alt") +
+        element.getAttribute("alt") +
         '" /></div><span class="lightbox__title">' +
-        title.substr(0, title.lastIndexOf(".")) +
+        title.substring(0, title.lastIndexOf(".")) +
         "</span>";
       lightbox.style.display = "block";
     });
