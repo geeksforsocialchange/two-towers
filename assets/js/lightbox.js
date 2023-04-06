e44048353b601b21bb0a76332663e52aa001d5df
“r-ferriergit config --global user.email “rosemary.ferrier@gmail.com <rosemary.ferrier@gmail.com>
2023-04-06 09:42:39 +0100

    attach click listener to parent div instead of image

--- assets/js/lightbox.js
+++ assets/js/lightbox.js
@@ -1,20 +1,29 @@
-document.addEventListener("DOMContentLoaded", function() {
+document.addEventListener("DOMContentLoaded", () => {
+  //hide the lightbox
+  document.querySelectorAll("div.lightbox")[0].addEventListener("click", () => {
+    this.innerHTML = "";
+    document.querySelectorAll("div.lightbox")[0].style.display = "none";
+  });
 
-    //hide the lightbox
-    document.querySelectorAll('div.lightbox')[0].addEventListener("click", function(event) {
-        this.innerHTML = '';
-        document.querySelectorAll('div.lightbox')[0].style.display = 'none';
+  //show the lightbox on click
+  const elements = document.querySelectorAll("img.lightbox-image__image");
+  const lightbox = document.querySelectorAll("div.lightbox")[0];
+  elements.forEach((element) => {
+    element.parentElement.addEventListener("click", (event) => {
+      event.preventDefault();
+      lightbox.innerHTML =
+        '<a class="lightbox__close"></a><div class="lightbox__image" style="background: url(\'' +
+        element.getAttribute("src") +
+        '\') center center / contain no-repeat;" title="' +
+        element.getAttribute("title") +
+        '" ><img class="lightbox__hidden-image" src="' +
+        element.getAttribute("src") +
+        '" alt="' +
+        element.getAttribute("alt") +
+        '" /></div><span class="lightbox__title">' +
+        element.getAttribute("title") +
+        "</span>";
+      lightbox.style.display = "block";
     });
-
-    //show the lightbox on click
-    const elements = document.querySelectorAll('img.lightbox-image__image');
-    const lightbox = document.querySelectorAll('div.lightbox')[0];
-    elements.forEach(element => {
-        element.addEventListener("click", function(event) {
-            event.preventDefault();
-            lightbox.innerHTML = '<a class="lightbox__close"></a><div class="lightbox__image" style="background: url(\''+this.getAttribute('src')+'\') center center / contain no-repeat;" title="'+this.getAttribute('title')+'" ><img class="lightbox__hidden-image" src="'+this.getAttribute('src')+'" alt="'+this.getAttribute('alt')+'" /></div><span class="lightbox__title">'+this.getAttribute('title')+'</span>';
-            lightbox.style.display = 'block';
-        });
-    });
-
+  });
 });
